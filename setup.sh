969d3a1fb9b69867f225da08dcce2592564ab704
ericsson-james <43553673+ericsson-james@users.noreply.github.com>
2019-09-28 20:10:08 -0400

    fixed the sources.list

--- setup.sh
+++ setup.sh
@@ -1,7 +1,19 @@
 echo  "Creating a backup of the sources.list and adding the rolling release regular repositories and source repositories"
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.BackUp
-echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" > /etc/apt/sources.list
-echo "deb-src http://http.kali.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
+
+echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/apt/sources.list
+echo "# For source package access, uncomment the following line" >> /etc/apt/sources.list
+echo "# deb-src http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
+echo "deb http://http.kali.org/kali sana main non-free contrib" >> /etc/apt/sources.list
+echo "deb http://security.kali.org/kali-security sana/updates main contrib non-free" >> /etc/apt/sources.list
+echo "# For source package access, uncomment the following line" >> /etc/apt/sources.list
+echo "# deb-src http://http.kali.org/kali sana main non-free contrib" >> /etc/apt/sources.list
+echo "# deb-src http://security.kali.org/kali-security sana/updates main contrib non-free" >> /etc/apt/sources.list
+echo "deb http://old.kali.org/kali moto main non-free contrib" >> /etc/apt/sources.list
+echo "# For source package access, uncomment the following line" >> /etc/apt/sources.list
+echo "# deb-src http://old.kali.org/kali moto main non-free contrib" >> /etc/apt/sources.list
+
+
 
 echo "Starting the apt-get installations"
 sudo apt install gobuster
