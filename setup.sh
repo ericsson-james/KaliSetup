d38ef5371525f653b2627e902efdf5ee5a349875
ericsson-james <43553673+ericsson-james@users.noreply.github.com>
2019-10-01 11:02:07 -0400

    added autoblue

--- setup.sh
+++ setup.sh
@@ -20,6 +20,7 @@ echo "Placing git repos into /opt"
 cd /opt
 sudo git clone https://github.com/Tib3rius/AutoRecon.git
 sudo git clone https://github.com/rebootuser/LinEnum.git
+sudo git clone https://github.com/3ndG4me/AutoBlue-MS17-010.git
 sudo git clone https://github.com/dreadlocked/Drupalgeddon2.git
 sudo gem install text-table
 sudo gem install highline
