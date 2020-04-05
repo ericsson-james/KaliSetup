83792de7329b91aae120cd2590203fc8f8498532
ericsson-james <ericsson_james@columbusstate.edu>
2020-04-05 19:28:03 -0400

    small modifications

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
-echo  "Creating a backup of the sources.list and adding the rolling release regular repositories and source repositories"
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.BackUp
+#echo  "Creating a backup of the sources.list and adding the rolling release regular repositories and source repositories"
+#sudo cp /etc/apt/sources.list /etc/apt/sources.list.BackUp
+#sudo echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/apt/sources.list
+#echo "# For source package access, uncomment the following line" >> /etc/apt/sources.list
+#echo "# deb-src http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
+#sudo echo "deb http://http.kali.org/kali sana main non-free contrib" >> /etc/apt/sources.list
+#sudo echo "deb http://security.kali.org/kali-security sana/updates main contrib non-free" >> /etc/apt/sources.list
+#echo "# For source package access, uncomment the following line" >> /etc/apt/sources.list
+#echo "# deb-src http://http.kali.org/kali sana main non-free contrib" >> /etc/apt/sources.list
+#echo "# deb-src http://security.kali.org/kali-security sana/updates main contrib non-free" >> /etc/apt/sources.list
+#echo "deb http://old.kali.org/kali moto main non-free contrib" >> /etc/apt/sources.list
+#echo "# For source package access, uncomment the following line" >> /etc/apt/sources.list
+#echo "# deb-src http://old.kali.org/kali moto main non-free contrib" >> /etc/apt/sources.list
 
-echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/apt/sources.list
-echo "# For source package access, uncomment the following line" >> /etc/apt/sources.list
-echo "# deb-src http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
-echo "deb http://http.kali.org/kali sana main non-free contrib" >> /etc/apt/sources.list
-echo "deb http://security.kali.org/kali-security sana/updates main contrib non-free" >> /etc/apt/sources.list
-echo "# For source package access, uncomment the following line" >> /etc/apt/sources.list
-echo "# deb-src http://http.kali.org/kali sana main non-free contrib" >> /etc/apt/sources.list
-echo "# deb-src http://security.kali.org/kali-security sana/updates main contrib non-free" >> /etc/apt/sources.list
-echo "deb http://old.kali.org/kali moto main non-free contrib" >> /etc/apt/sources.list
-echo "# For source package access, uncomment the following line" >> /etc/apt/sources.list
-echo "# deb-src http://old.kali.org/kali moto main non-free contrib" >> /etc/apt/sources.list
+
 
 echo "Starting the update"
 sudo apt-get update
@@ -23,23 +24,19 @@ sudo git clone https://github.com/rebootuser/LinEnum.git
 sudo git clone https://github.com/3ndG4me/AutoBlue-MS17-010.git
 sudo git clone https://github.com/tennc/webshell.git
 sudo git clone https://github.com/dreadlocked/Drupalgeddon2.git
-sudp git clone https://github.com/jivoi/pentest.git ./offsecfw
+sudo git clone https://github.com/jivoi/pentest.git ./offsecfw
 sudo gem install text-table
 sudo gem install highline
-cd AutoRecon
-pip3 install -r requirements.txt
-cd /opt
 
+pip3 install -r AutoRecon/requirements.txt
 echo "Starting the apt-get installations"
-sudo apt install gobuster
-sudo apt install mingw-w64
-sudo apt install veil
-sudo apt install sublist3r
-sudo apt install seclists
-sudo apt install ftp
-sudo apt install p7zip-full
-sudo apt install exiftool
-sudo apt install -y virtualbox-guest-x11
-sudo apt install libguestfs-tools
+sudo apt install -y gobuster
+#sudo apt install mingw-w64
+#sudo apt install veil
+sudo apt install -y sublist3r
+sudo apt install -y seclists
+sudo apt install -y ftp
+sudo apt install -y p7zip-full
+sudo apt install -y exiftool
 echo "updating the db"
 sudo updatedb
